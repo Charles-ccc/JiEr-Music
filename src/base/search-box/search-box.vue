d5b7a645dbd73ca539bfee7d476ba52852d902e9
Charles-ccc <liu930505@qq.com>
2018-09-29 20:34:43 +0800

    halo

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="search-box">
+        <i class="icon-search"></i>
+        <input type="text" class="box" v-model="query" :placeholder="placeholder">
+        <i class="icon-dismiss" v-show="query" @click="clear"></i>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            placeholder: {
+                type:String,
+                default: "搜索歌曲、歌手"
+            }
+        },
+        data() {
+            return {
+                query: ''
+            }
+        },
+        clear() {
+            this.query = ''
+        },
+        created () {
+            // 需要把query给暴露出去
+            this.$watch('query', (newQuery) => {
+                this.$emit('query', newQuery)
+            })
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import "../../common/stylus/variable"
+
+  .search-box
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    width: 100%
+    padding: 0 6px
+    height: 40px
+    background: $color-highlight-background
+    border-radius: 6px
+    .icon-search
+      font-size: 24px
+      color: $color-background
+    .box
+      flex: 1
+      margin: 0 5px
+      line-height: 18px
+      background: $color-highlight-background
+      color: $color-text
+      font-size: $font-size-medium
+      &::placeholder
+        color: $color-text-d
+    .icon-dismiss
+      font-size: 16px
+      color: $color-background
+</style>
